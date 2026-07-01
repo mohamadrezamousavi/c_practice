cda512eaee1cfba4d7c38638e7f4f30b2518ed0e
Mohamadreza Mousavi <mohamadreza.mousavi.20@gmail.com>
2026-07-02 00:53:13 +0330

    learning: finding highest grade function is added to practice 18

--- practice_18.c
+++ practice_18.c
@@ -2,23 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-    typedef struct
-    {
-        char name[30];
-        int age;
-        float grade;
-    } Student;
+typedef struct
+{
+    char name[30];
+    int age;
+    float grade;
+} Student;
 void inputStudents(Student *ptr, int size);
 void printStudents (Student *ptr, int size);
+Student *highestGrade (Student *ptr, int size);
+void printHighest (Student *ptr);
 
 int main()
 {
 
 
-    Student students[3];
+    Student students[5];
     Student *ptr = students;
-    inputStudents (ptr, 3);
-    printStudents (ptr, 3);
+    inputStudents (ptr, 5);
+    printStudents (ptr, 5);
+    Student *highest =highestGrade(ptr,2);
+    printHighest(highest);
     return 0;
 }
 
@@ -48,4 +52,22 @@ void printStudents (Student *ptr, int size)
     {
         printf("%-5d %-20s %-5d %-7.2f\n", i +1,(ptr+i) ->name,(ptr+i)->age,(ptr+i)->grade);
     }
+}
+
+void printHighest (Student *ptr)
+{
+    printf("\nHighest Grade Student is: %s with %.2f\n",ptr->name,ptr->grade);
+}
+
+Student *highestGrade (Student *ptr, int size)
+{
+    Student *highest_ptr;
+    highest_ptr = ptr;
+    for (int i = 1; i < size; i++)
+    {
+        if ((ptr+i) -> grade > highest_ptr ->grade)
+           highest_ptr  = (ptr+i)  ; 
+    }
+
+    return highest_ptr;
 }
